0dca03a17028857238ba354ba423fb173a9ae4cd
Omri Asher <omriash4@gmail.com>
2025-11-21 22:36:35 +0200

    remove comments

--- server/main.py
+++ server/main.py
@@ -5,7 +5,6 @@ app = FastAPI()
 
 class ConnectionManager:
     def __init__(self):
-        # מילון: המפתח הוא שם החדר, הערך הוא רשימת המחוברים לחדר
         self.rooms: Dict[str, List[WebSocket]] = {}
 
     async def connect(self, websocket: WebSocket, room_id: str):
@@ -18,7 +17,7 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket, room_id: str):
         if room_id in self.rooms:
             self.rooms[room_id].remove(websocket)
-            if not self.rooms[room_id]: # אם החדר ריק, נמחק אותו
+            if not self.rooms[room_id]: 
                 del self.rooms[room_id]
         print(f"User disconnected from room: {room_id}")
 
@@ -30,14 +29,12 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
-# הכתובת השתנתה! עכשיו מקבלים גם room_id
 @app.websocket("/ws/{room_id}")
 async def websocket_endpoint(websocket: WebSocket, room_id: str):
     await manager.connect(websocket, room_id)
     try:
         while True:
             data = await websocket.receive_text()
-            # שולח רק לאנשים באותו חדר
             await manager.broadcast(data, websocket, room_id)
     except WebSocketDisconnect:
         manager.disconnect(websocket, room_id)
